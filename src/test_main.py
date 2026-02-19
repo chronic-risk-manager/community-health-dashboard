9a19f2b7a33ae9c0e7b2e4a56e608a3a23225a4c
t3cami00 <t3cami00@students.oamk.fi>
2026-02-19 12:18:52 +0200

    Fix test bugs

--- src/test_main.py
+++ src/test_main.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
